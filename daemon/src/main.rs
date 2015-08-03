d35cff2142e88632cae6484371f69eeef619bf6e
frederik <f.bosch@genkgo.nl>
2015-08-03 15:39:11 +0200

    receive from stomp, execute command

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,36 +1,81 @@
 extern crate stomp;
+extern crate rustc_serialize;
 
+use rustc_serialize::json;
+use std::process::Command;
+use std::sync::mpsc::channel;
+use std::thread;
 use stomp::frame::Frame;
 use stomp::header::{Header, SuppressedHeader};
 use stomp::connection::{Credentials};
 use stomp::subscription::AckOrNack::Ack;
+use stomp::subscription::AckMode;
+
+const NUMBER_OF_THREADS : u32 = 10;
+const TOPIC : &'static str = "/queue/test_messages";
+
+#[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
+struct WorkerItem {
+	id: String,
+	command: String,
+	cwd: String
+}
 
 fn main() {
+	let mut threads = vec![];
+	let (result_backend_tx, result_backend_rx) = channel::<WorkerItem>();
 
-  let destination = "/topic/messages";
-  let mut message_count: u64 = 0;
+	for thread_number in 0..NUMBER_OF_THREADS {
+		let tx = result_backend_tx.clone();
+		let processor = thread::spawn(move || {
+			let mut session = match stomp::session("172.17.0.4", 61613)
+				.with(Credentials("guest", "guest"))
+				.with(SuppressedHeader("host"))
+				.with(Header::new("host", "/"))
+				.start() {
+					Ok(session) => session,
+					Err(error)  => panic!("Could not connect to the server: {} {}", error, thread_number)
+				}
+			;
 
-  let mut session = match stomp::session("172.17.0.6", 61613)
-    .with(Credentials("guest", "guest"))
-    .with(SuppressedHeader("host"))
-    .with(Header::new("host", "/"))
-    .start() {
-      Ok(session) => session,
-      Err(error)  => panic!("Could not connect to the server: {}", error)
-   };
+			println!("started session {:?}", thread_number);
 
-  session.subscription(destination, |frame: &Frame| {
-    message_count += 1;
-    println!("Received message {}", frame);
-    Ack
-  }).start().ok().expect("unable to receive message");
+			session.subscription(TOPIC, |frame: &Frame| {
+				let thread_number = thread_number.clone();
+				let frame_body = String::from_utf8(frame.body.clone()).ok().expect("cannot convert frame body to string");
+				let worker: WorkerItem = json::decode(&frame_body).unwrap();
 
-  let send_error = "unable to send message";
-  session.message(destination, "Animal").send().ok().expect(send_error);
-  session.message(destination, "Vegetable").send().ok().expect(send_error);
-  session.message(destination, "Mineral").send().ok().expect(send_error);
+				println!("Will execute {} for cwd {} in thread {}", worker.command, worker.cwd, thread_number);
+				let output = Command::new("sh")
+					.arg("-c")
+					.arg(worker.command.clone())
+					.current_dir(worker.cwd.clone())
+					.output()
+					.unwrap_or_else(|e| {
+						panic!("failed to execute process: {}", e);
+					});
+					println!("{:?}", String::from_utf8_lossy(&output.stdout));
+					println!("{:?}", &output.status.code().unwrap());
+					tx.send(worker.clone()).unwrap();
+					Ack
+				})
+				.with(AckMode::ClientIndividual)
+				.with(Header::new("prefetch-count", "1"))
+				.start().ok().expect("unable to start receiving messages")
+			;
+			session.listen().ok().expect("unable to listen"); // Loops infinitely, awaiting messages
+		});
+		threads.push(processor);
+	}
 
-  session.listen().ok().expect("unable to listen"); // Loops infinitely, awaiting messages
+	for item in threads {
+		item.join().ok().expect("unable to join processor thread");
+	}
 
-  session.disconnect().ok().expect("cannot disconnect from server");
+	let result_backend = thread::spawn(move || {
+		let worker = result_backend_rx.recv().unwrap();
+		println!("Processed message {}", worker.command);
+	});
+
+	result_backend.join().ok().expect("unable to join result backend thread");
 }
