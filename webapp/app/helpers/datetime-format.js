b978989cd832bfa71c7d95cb91be2c1641866fa2
frederik <f.bosch@genkgo.nl>
2015-09-17 11:14:36 +0200

    add start date to job table, use locale based dates with moment

--- webapp/app/helpers/datetime-format.js
+++ webapp/app/helpers/datetime-format.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Handlebars.makeBoundHelper(function(date) {
-  return moment(date).format('MM-DD-YYYY HH:mm');
+  return moment(date).format('LLL');
 });
