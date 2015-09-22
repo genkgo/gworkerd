edc28b498ae51fa61b32a4fea4654ce52bf3505a
Frederik Bosch <f.bosch@genkgo.nl>
2015-09-22 16:05:32 +0200

    webapp ordering jobs

--- webapp/app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  reverse: function(){
-    return this.get('model').toArray().reverse();
-  }.property('model.[]')
-});
--- webapp/app/controllers/jobs.js
+++ webapp/app/controllers/jobs.js
@@ -1,8 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['status'],
-  reverse: function(){
-    return this.get('model').toArray().reverse();
-  }.property('model.[]')
+  queryParams: ['status']
 });
